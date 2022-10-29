7d6491c20960e82de2e3029e76c10d1dd4a4432f
Jacob Girling <jacobgirling@Jacobs-MacBook-Pro.local>
2022-10-29 17:29:34 +0100

    last login

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useQuery} from "react-query";
+import axios from "axios";
+import {baseUrl} from "../../utils/constants";
 
 const Footer = () => {
+
+ const [date, setDate] = useState<Date>()
+
+  useQuery<string>("getLastLogin", async () => {
+    const response = await axios.get(`${baseUrl}/last-login`)
+    return response.data
+  }, {
+    onSuccess: login => {
+      setDate(new Date(login))
+    }
+  })
+
   return (
     <footer className="absolute block bottom-0 left-0 bg-gray-300 dark:bg-slate-700 min-w-full">
-      <div className="flex justify-end text-black dark:text-slate-300 px-2">
-        <p>Last Login: 2022-09-15 12:00:00</p>
-      </div>
+      {date && (
+        <div className="flex justify-end text-black dark:text-slate-300 px-2">
+          <p>Last Login: {date.toDateString()}</p>
+        </div>
+      )}
     </footer>
   )
 }
