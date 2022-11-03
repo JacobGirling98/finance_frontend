d4eee9d3374d67b04d21a7e6b5fa32d86d10e583
JacobGirling98 <jacob.girling98@gmail.com>
2022-11-03 21:24:28 +0000

    git sync

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -16,7 +16,7 @@ const Spinner: FC<SpinnerProps> = (
     <>
       {isOpen && (
         <>
-          {muteBackground && <div className="w-full h-full bg-gray-500/75 fixed inset-0 opacity-70 transition-opacity z-10"/>}
+          {muteBackground && <div className="w-full h-full bg-gray-500/75 fixed inset-0 opacity-70 transition-opacity duration-300 z-10"/>}
           <div className="absolute top-[50%] left-[50%] -ml-10 -mt-10">
             <span className="h-10 w-10 block rounded-full border-2 border-t-indigo-500 animate-spin z-20"/>
           </div>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,22 +1,36 @@
-import { BanknotesIcon } from "@heroicons/react/24/outline";
+import {BanknotesIcon} from "@heroicons/react/24/outline";
 import NavButtons from "../../navigation/NavButtons";
+import {useMutation} from "react-query";
+import axios from "axios";
+import {baseUrl} from "../../utils/constants";
+import Spinner from "../Spinner";
 
 const Navbar = () => {
+
+  const {mutate, isLoading} = useMutation("gitSync", async () => await axios.post(`${baseUrl}/git/sync`, {}))
+
   return (
-    <nav className="bg-cyan-100 dark:bg-gray-800">
-      <div className="flex py-2 mx-2">
-        <div className="flex text-gray-300 font-bold text-xl pl-2 pr-8 items-center">
-          <p className="pr-2">My Finances</p>
-          <BanknotesIcon className="block h-6 w-6" aria-hidden={true} />
+    <>
+      <Spinner isOpen={isLoading} muteBackground={true}/>
+      <nav className="bg-cyan-100 dark:bg-gray-800">
+        <div className="flex py-2 mx-2">
+          <div className="flex text-gray-300 font-bold text-xl pl-2 pr-8 items-center">
+            <p className="pr-2">My Finances</p>
+            <BanknotesIcon className="block h-6 w-6" aria-hidden={true}/>
+          </div>
+          <NavButtons
+            className="px-2 py-2 rounded-md text-sm font-medium transition duration-150 ease-in-out text-gray-300 hover:bg-gray-700 hover:text-white"/>
+          <div className="flex items-center ml-auto">
+            <button
+              className="px-2 bg-indigo-700 rounded-md text-base font-medium text-white h-full w-28 hover:bg-indigo-600 active:bg-indigo-500 transition duration-150 ease-in-out"
+              onClick={() => mutate()}
+            >
+              <span>Sync</span>
+            </button>
+          </div>
         </div>
-        <NavButtons className="px-2 py-2 rounded-md text-sm font-medium transition duration-150 ease-in-out text-gray-300 hover:bg-gray-700 hover:text-white" />
-        <div className="flex items-center ml-auto">
-          <button className="px-2 bg-indigo-700 rounded-md text-base font-medium text-white h-full w-28 hover:bg-indigo-600 active:bg-indigo-500 transition duration-150 ease-in-out">
-            <span>Sync</span>
-          </button>
-        </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 
