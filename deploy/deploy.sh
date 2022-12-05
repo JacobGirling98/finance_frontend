2c61e4ee9ad112a103b2b5aa159fc095cfeafb49
JacobGirling98 <jacob.girling98@gmail.com>
2022-12-05 22:13:58 +0000

    bit more deploying

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-docker build -t finance-frontend .
+docker build -t finance-frontend ../.
 docker save finance-frontend | gzip -c > frontend.tar.gz
 
 scp frontend.tar.gz pi:~/Programming/finance/frontend.tar.gz
 
-#rm frontend.tar.gz
+rm frontend.tar.gz
 
 finance_dir=/home/jacobg/Programming/finance
 
 ssh pi "cd $finance_dir && docker load < frontend.tar.gz"
-#ssh pi "cd $finance_dir rm frontend.tar.gz"
+ssh pi "cd $finance_dir rm frontend.tar.gz"
 ./start-container.sh
--- deploy/start-container.sh
+++ deploy/start-container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh pi "docker run -p 3000:3000 -t finance-frontend"
+ssh pi "docker run -p 3000:3000 -d --name finance-frontend finance-frontend"
