a43d3b04212378402e58e6f322f0d736979ddfe9
Jacob Girling <jacobgirling@Jacobs-MacBook-Pro.local>
2022-12-20 15:09:22 +0000

    theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,19 +30,73 @@ module.exports = {
             main: colors.gray[900]
           }
         },
+        bg: {
+          light: "#ffffff",
+          dark: "#202020"
+        },
         text: {
-          primary: {
-            dark: {
-              main: colors.gray[500],
-              hover: colors.gray[400],
-              active: colors.gray[300]
-            }
-          }
+          light: "#273040",
+          dark: "#dddfe4"
+        },
+        'text-soft': {
+          light: "#444a56",
+          dark: "#b9bbc1"
+        },
+        'text-strong': {
+          light: "#4a4a4a",
+          dark: "#bbbbbb"
+        },
+        subtle: {
+          light: "#f8f9fd",
+          dark: "#222429"
+        },
+        border: {
+          light: "#e0e7f6",
+          dark: "#2a313d"
+        },
+        shadow: {
+          light: "#b0c0dd",
+          dark: "#000000"
         },
         input: {
-          primary: {
-            dark: colors.gray[600]
-          }
+          light: "#ffffff",
+          dark: "#1c1c1c"
+        },
+        outline: {
+          light: "#bebbe5",
+          dark: "#9c98d7"
+        },
+        mark: {
+          light: "#7a71de14",
+          dark: "#8882ce14"
+        },
+        special: {
+          light: "#7a71de",
+          dark: "#8882ce"
+        },
+        "special-bg": {
+          light: "#7e76df",
+          dark: "#5e56b6"
+        },
+        "special-text": {
+          light: "#ffffff",
+          dark: "#dfdfe4"
+        },
+        "special-shadow": {
+          light: "#585773",
+          dark: "#1e1d29"
+        },
+        "special-mark": {
+          light: "#ffffff14",
+          dark: "#dfdfe414"
+        },
+        light: {
+          light: "#f3f3fc",
+          dark: "#d3d1ed"
+        },
+        dark: {
+          light: "#45426c",
+          dark: "#343347"
         }
       },
     },
