b3f8cced7f526020d845d4e5c09e840712f51b2d
JacobGirling98 <jacob.girling98@gmail.com>
2023-11-16 16:27:56 +0000

    pi on new network

--- deploy/run/run-app-pi.sh
+++ deploy/run/run-app-pi.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 container_name="finance-frontend"
 
+container_id=$(docker ps -q -f name=$container_name)
+
+if [ -n "$container_id" ]; then
+  docker container stop $container_name
+fi
+
+docker rm $container_name
+
 docker run -p 3000:3000 -d --name $container_name -t $container_name
