d0c327ab762bdb45501fcac96ff2f6b4b055c70a
Mona Lisa <jacob.girling98@gmail.com>
2024-01-06 17:21:22 +0000

    transition

--- src/components/utils/Snackbar/Snackbar.tsx
+++ src/components/utils/Snackbar/Snackbar.tsx
@@ -1,3 +1,4 @@
+import { Transition } from "@headlessui/react"
 import { ChildrenProps } from "../../../types/ChildrenProps"
 
 interface SnackbarProps {
@@ -7,9 +8,18 @@ interface SnackbarProps {
 const Snackbar = ({ isOpen, children }: SnackbarProps & ChildrenProps) => {
   return (
     <>
-      {isOpen && (
+      <Transition
+        appear={true}
+        show={isOpen}
+        enter="transition-opacity duration-500"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition-opacity duration-500"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
         <div className="fixed bottom-11 right-5 flex shadow-xl">{children}</div>
-      )}
+      </Transition>
     </>
   )
 }
